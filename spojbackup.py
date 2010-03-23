abdb2294e540a3d83ea11fa4fc0bfbb7f2181029
Abhishek Mishra <ideamonk@gmail.com>
2010-03-23 21:55:49 +0530

    merged shashwat's changes, removed argparse dependency

--- spojbackup.py
+++ spojbackup.py
@@ -8,12 +8,13 @@
             |__   |   __|  |  |  |  |  | . | .'|  _| '_| | | . |
             |_____|__|  |_____|_____|  |___|__,|___|_,_|___|  _|
                                                            |_|
-                                    by Abhishek Mishra <ideamonk >< gmail.com >
-								      Shashwat Anand <anand.shashwat gmail.com>
+                                    by Abhishek Mishra      <ideamonk@gmail.com >
+                                       Shashwat Anand  <anand.shashwat@gmail.com> 
 
+Keywords: python, tools, algorithms, spoj
 
-Copyright (c) 2009, Abhishek Mishra
-All rights reserved.
+Copyright (C) 2003-2004 Free Software Foundation, Inc.
+
 
 Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
@@ -37,42 +38,55 @@ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
 ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 '''
+# Dependencies:
+# mechanize => http://pypi.python.org/pypi/mechanize/0.1.7b
 
-import getpass, os, glob
+import os
+import sys
+import glob
+import getpass
+import optparse
 
-try:
-    import argparse
-except ImportError:
-    print "argparse required but missing"
-    sys.exit(1)
 
 try:
     from mechanize import Browser
 except ImportError:
-    print "mechanize is required but missing"
+    print "mechanize required but missing"
     sys.exit(1)
     
 
-def getSolutions (path_prefix):
+def getSolutions (path_prefix, path_proxy):
+    global br, username, password
+
+    # create a browser object
     br = Browser()
+
+    # add proxy support to browser
+    if len(path_proxy) != 0: 
+        protocol,proxy = options.proxy.split("://")
+        br.set_proxies({protocol:proxy})
     
-    # make me a browser that makes ass of robots.txt
-    br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+    # let browser fool robots.txt
+    br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; \
+              rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
     br.set_handle_robots(False)
 
     print "Enter yout SPOJ username :",
     username = raw_input()
     password = getpass.getpass()
 
-    # Authenticate the user
+    # authenticate the user
     print "Authenticating " + username
     br.open ("http://spoj.pl")
     br.select_form (name="login")
     br["login_user"] = username
     br["password"] = password
-    # sign em up for a day for avoiding timeouts
+
+    # sign in for a day to avoid timeouts
     br.find_control(name="autologin").items[0].selected = True
+    br.form.action = "http://www.spoj.pl"
     response = br.submit()
 
     verify = response.read()
@@ -80,37 +94,42 @@ def getSolutions (path_prefix):
         print "Error authenticating - " + username
         exit(0)
 
-    # grab the signed list of submissions
+    # grab the signed submissions list
     print "Grabbing siglist for " + username
-    siglist = br.open("https://www.spoj.pl/status/" + username + "/signedlist")
+    siglist = br.open("http://www.spoj.pl/status/" + username + "/signedlist")
 
-    # dump first 9 lines
+    # dump first nine useless lines in signed list for formatting
     for i in xrange(9):
-        crap = siglist.readline()
+        siglist.readline()
 
     # make a list of all AC's and challenges
     print "Filtering siglist for AC/Challenge solutions..."
     mysublist = list()
 
-    while (1):
+    while True: 
         temp = siglist.readline()
         
         if temp=='\------------------------------------------------------------------------------/\n':
             # reached end of siglist
             break
 
-        if (len(temp)==0):
+        if not len(temp) :
             print "Reached EOF, siglist format has probably changed," + \
                     " contact author."
             exit(1)
             
         entry = [x.strip() for x in temp.split('|')]
         
-        if (entry[4] == 'AC' or entry[4].isdigit()):
+        if entry[4] == 'AC' or entry[4].isdigit():
             mysublist.append (entry)
 
-    totalsubmissions = len(mysublist)
+    print "Done !!!"
+    return mysublist
 
+def downloadSolutions(mysublist):
+    
+    totalsubmissions = len(mysublist)
+    
     print "Fetching sources into " + path_prefix
     progress = 0
     
@@ -119,10 +138,10 @@ def getSolutions (path_prefix):
                                                            (entry[3],entry[1])))
 
         progress += 1
-        if (len(existing_files)>0):
+        if len(existing_files) == 1:
             print "%d/%d - %s skipped." % (progress, totalsubmissions, entry[3])
         else:
-            source_code = br.open("https://www.spoj.pl/files/src/save/" + \
+            source_code = br.open("http://www.spoj.pl/files/src/save/" + \
                                                                        entry[1])
             header = dict(source_code.info())
             filename = ""
@@ -142,14 +161,13 @@ def getSolutions (path_prefix):
 
 
 if __name__=="__main__":
-    parser = argparse.ArgumentParser(prog="spojbackup",
-            description = "Creates a backup of all your Submissions on SPOJ " +\
-            "(Sphere Online Judge) http://spoj.pl in a desired place")
 
-    parser.add_argument("-o", "--outputpath",default="./", type=str,
-                              help="Directory to store all fetched solutions")
+    parser = optparse.OptionParser()
+    parser.add_option("-o", "--outputpath", default="./", type=str, help="Destination directory to store solutions")
+    parser.add_option("-p", "--proxy", default = "", type = str, help = "Proxy , ex- http://username:password@proxy:port")
 
-    args = parser.parse_args()
-
-    getSolutions (args.outputpath)
+    (options, args) = parser.parse_args()
+    path_prefix = options.outputpath 
+    path_proxy = options.proxy   
     
+    downloadSolutions(getSolutions(path_prefix, path_proxy))
